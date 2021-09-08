c7051d88cd62d28244806414728e9213e294ca7f
eyalBarGit <eyalb.media@gmail.com>
2021-09-08 20:22:57 +0300

    feat(navBar): finish desktop layout

--- frontend/src/cmps/NavBar/NavBar.jsx
+++ frontend/src/cmps/NavBar/NavBar.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import logo from '../../assets/logo_black.png'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import InfoIcon from '@material-ui/icons/Info';
+import PeopleIcon from '@material-ui/icons/People';
+import SportsEsportsIcon from '@material-ui/icons/SportsEsports';
+
 export function NavBar() {
     const state = useSelector(state => state.state)
 
@@ -19,11 +24,11 @@ export function NavBar() {
                 <img style={{ width: '120px' }} src={logo} alt="" />
             </div>
 
-            <ul className="nav-bar-btns flex">
-                <li><button>Play 1 vs 1</button></li>
-                <li><button>1 vs Machine</button></li>
-                <li><button>About</button></li>
-                <li><button>Sign in</button></li>
+            <ul className="nav-bar-btns flex align-center">
+                <li><button> <span><PeopleIcon /></span>Play 1 vs 1</button></li>
+                <li><button> <span><SportsEsportsIcon /></span>1 vs Machine </button></li>
+                <li><button> <span><InfoIcon />About</span> </button></li>
+                <li><button> <span> <AccountCircleIcon /></span>Sign in</button></li>
             </ul>
 
         </div>
--- frontend/src/cmps/PlayModal/PlayModal.jsx
+++ frontend/src/cmps/PlayModal/PlayModal.jsx
@@ -1,24 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import heroImg from '../../assets/hero-img.png'
 import styled, { keyframes } from 'styled-components';
 import { bounceIn, fadeInLeft, fadeInRight } from 'react-animations';
 
 const bounceAnimation = keyframes`${bounceIn}`;
 const fadeInLeftAnimation = keyframes`${fadeInLeft}`;
+const fadeInRightAnimation = keyframes`${fadeInRight}`;
 
 export function PlayModal() {
-    const state = useSelector(state => state.state)
     const [isShow, setShow] = useState(false)
+    const [isRightShow, setRightShow] = useState(false)
+
 
     useEffect(() => {
         setTimeout(() => {
             setShow(true)
-        }, 1000);
-        // component didMount
-        return () => {
-            // component willUnMount
-        }
+            setTimeout(() => {
+                setRightShow(true)
+            }, 500);
+        }, 500);
     }, [])
 
 
@@ -26,12 +26,17 @@ export function PlayModal() {
         <div style={{ width: '100%' }} className="play-modal">
             <BouncyDiv className="flex justify-center align-center">
                 <img style={{ width: '250px' }} src={heroImg} alt="hero" />
-            </BouncyDiv  >
+            </BouncyDiv>
             <div style={{ height: '50px' }}>
                 {isShow &&
                     <FadeLeft className="flex justify-center align-center">
-                        <h2>1vs1</h2>
+                        <button>1 vs machine</button>
                     </FadeLeft>
+                }
+                {isRightShow &&
+                    <FadeRight className="flex justify-center align-center">
+                        <button  >1 vs 1</button>
+                    </FadeRight>
                 }
             </div>
         </div>
@@ -43,6 +48,11 @@ const BouncyDiv = styled.div`
   `;
 const FadeLeft = styled.div`
 animation: 0.5s ${fadeInLeftAnimation};
+width:100%;
+
+`
+const FadeRight = styled.div`
+animation: 0.5s ${fadeInRightAnimation};
 width:100%;
 
 `
